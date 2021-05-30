4cd8636e9fb13444714cc4240898c053b17852cd
mtorcellini <matt.torcellini@gmail.com>
2021-05-29 20:27:50 -0400

    about styles

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,18 +1,26 @@
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 function NavTabs() {
+
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link to="/">About</Link>
-      </li>
-      <li>
-        <Link to="/discover">Discover</Link>
-      </li>
-      <li>
-        <Link to="/search">Search</Link>
-      </li>
-    </ul>
+    <nav className="navbar bg-light navbar-light">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/">Pupster</Link>
+        </div>
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className={useLocation().pathname === "/" ? "nav-link active" : "nav-link"} to="/">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className={useLocation().pathname === "/discover" ? "nav-link active" : "nav-link"} to="/discover">Discover</Link>
+          </li>
+          <li className="nav-item">
+            <Link className={useLocation().pathname === "/search" ? "nav-link active" : "nav-link"} to="/search">Search</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 
 }
